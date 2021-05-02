a01062e8a3737880df279dc063ea3bad68c196c0
matthias pineau <pineau.matthias@gmail.com>
2021-05-02 16:05:41 +0200

    ajout des styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  Bonjour
-  <span>salut</span>
+  <div id="app" :class="{ 'moon' : theme == 'moon' }">
+
+    <!-- Navbar -->
+     <nav class="nav">
+       <div class="navbar">
+         <div class="navbar__brand">
+          <img :src="require('@/assets/images/logo-app.svg')" alt="logo">  
+         </div>
+         <div class="navbar__theme">
+           <img v-if="theme == 'sun'"
+            @click="switchThemes"
+           :src="require('@/assets/images/icon-sun.svg')" alt="icon sun">
+           <img  v-if="theme == 'moon'"
+           @click="switchThemes"
+           :src="require('@/assets/images/icon-moon.svg')" alt="icon moon">
+         </div>
+         <div class="navbar__avatar">
+           <img :src="require('@/assets/images/image-avatar.jpg')" alt="avatar">
+         </div>
+       </div>
+     </nav>
+
+    <!-- Main -->
+     <main class="main container">
+
+        <!-- Header -->
+        <header class="header">
+          <div class="header__heading">
+            <h1>Invoices</h1>
+            <span class="text--secondary">{{ nbInvoices }}</span>
+          </div>
+
+          <div class="header__filter">
+            Filter by statut
+          </div>
+
+          <button class="btn btn__2">New invoices</button>
+        </header>
+
+        <!-- Section Central -->
+        <section class="central">
+          <!-- Empty -->
+          <div v-if="db.length < 1" class="empty">
+            <div class="empty__media">
+              <img :src="require('@/assets/images/illustration-empty.svg')" alt="empty img">
+            </div>
+            <div class="empty__text">
+              <h3>There is nothing here</h3>
+              <p class="text--secondary">Create an invoice by clicking the <br>New Invoice button and get started</p>
+            </div>
+          </div>
+          <div v-else>
+            <div class="wrap__invoices">
+              invoices
+            </div>
+          </div>
+        </section>
+
+    
+
+     </main>
+
   </div>
 </template>
 
@@ -10,6 +68,23 @@
 
 
 export default {
+  data() {
+    return {
+      theme: 'sun',
+      db: [],
+      
+    }
+  },
+  methods: {
+    switchThemes() {
+      this.theme = this.theme == 'sun' ? 'moon' : 'sun'
+    }
+  },
+  computed: {
+    nbInvoices() {
+      return this.db.length < 1 ? 'No invoices' : this.db.length + ' invoices'
+    } 
+  },
   name: 'App',
   components: {
   }
